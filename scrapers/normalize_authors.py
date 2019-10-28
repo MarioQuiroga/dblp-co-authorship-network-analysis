e1f93c3203ba57ac1f66b1669a3cef9107283e6d
agrup <agustinrodriguez206@gmail.com>
2019-10-27 23:43:17 -0300

    change utf-t to ascii

--- scrapers/normalize_authors.py
+++ scrapers/normalize_authors.py
@@ -10,7 +10,7 @@ normalize_authors=[]
 with open(file_all_authors,'r')as authors:
     csv_reader = csv.reader(authors, delimiter=',')
     for author in csv_reader:
-        normalize_authors.append((ud.normalize('NFD', author[0]).encode('Utf-8', 'ignore').decode(),author[1]))
+        normalize_authors.append((ud.normalize('NFD', author[0]).encode('ascii', 'ignore').decode(),author[1]))
 
 with open(file_all_authors_save,'w') as file_save:
     for author,org in normalize_authors:
