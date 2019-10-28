1d392b6086d76000ac6affbdad398021cdeb5528
agrup <agustinrodriguez206@gmail.com>
2019-10-27 23:30:08 -0300

    normalize authors

---
+++ scrapers/normalize_authors.py
@@ -0,0 +1,17 @@
+import unicodedata as ud
+import csv
+import os
+
+path= os.getcwd()+'/data'
+file_all_authors=path+'/Authors.csv'
+file_all_authors_save=path+'/Authoers_normalize.csv'
+normalize_authors=[]
+
+with open(file_all_authors,'r')as authors:
+    csv_reader = csv.reader(authors, delimiter=',')
+    for author in csv_reader:
+        normalize_authors.append((ud.normalize('NFD', author[0]).encode('Utf-8', 'ignore').decode(),author[1]))
+
+with open(file_all_authors_save,'w') as file_save:
+    for author,org in normalize_authors:
+        file_save.write(author+','+org+'\n')
