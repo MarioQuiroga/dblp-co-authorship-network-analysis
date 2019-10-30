226751f311b6d47bc0e876f42d705dfbeaf827c6
agrup <agustinrodriguez206@gmail.com>
2019-10-30 16:56:29 -0300

    add html number to google researchers

--- scrapers/google_researchers_scraper.py
+++ scrapers/google_researchers_scraper.py
@@ -5,12 +5,13 @@ import csv
 
 url = "https://ai.google/static/data/authors.json"
 organitation= 'Google'
+url_titles = 'https://ai.google/research/people/'
 os.getcwd()
 
 #json file 
 savefile = os.getcwd()+'/data/google/google_authors.json'
 #csv file (name,organitation)
-savecsv= os.getcwd()+'data/google/google_authors.csv'
+savecsv= os.getcwd()+'/data/google/google_authors.csv'
 
 response = requests.request("GET", url)
 
@@ -21,17 +22,24 @@ print(response.encoding)
 content =response.content
 jsoncontent = json.loads(response.content)
 
+with open(savefile, 'wb') as f:
+    f.write(response.content)
+print("file saved")
+
+
+authors_json={}
 authorscsv=[]
 for author in jsoncontent['authors']:
-    authorscsv.append([author['name'],organitation])
-    print(author['name'],organitation)
+    data = {}
+    authorscsv.append([author['name'],organitation,author['filename_html']])
+    data['afiliation']=organitation
+    data['html']=author['filename_html']
+    authors_json[author['name']]=data
+    print(author['name'],organitation,author['filename_html'])
 
 with open(savecsv, 'w') as f:
     writer = csv.writer(f)
     writer.writerows(authorscsv)    
 
 
-with open(savefile, 'wb') as f:
-    f.write(response.content)
-print("file saved")
 
