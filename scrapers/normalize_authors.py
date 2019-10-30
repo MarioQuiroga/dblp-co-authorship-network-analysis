8dccaed0f059872f15992488cc0d8c9831cb4409
agrup <agustinrodriguez206@gmail.com>
2019-10-30 01:00:46 -0300

    remove asian names

--- scrapers/normalize_authors.py
+++ scrapers/normalize_authors.py
@@ -1,6 +1,7 @@
 import unicodedata as ud
 import csv
 import os
+import re
 
 path= os.getcwd()+'/data'
 file_all_authors=path+'/Authors.csv'
@@ -10,7 +11,8 @@ normalize_authors=[]
 with open(file_all_authors,'r')as authors:
     csv_reader = csv.reader(authors, delimiter=',')
     for author in csv_reader:
-        normalize_authors.append((ud.normalize('NFD', author[0]).encode('ascii', 'ignore').decode(),author[1]))
+        normalize_author= re.sub(r'[^A-Za-z]+', ' ', ud.normalize('NFD', author[0]).encode('ascii', 'ignore').decode())
+        normalize_authors.append((normalize_author,author[1]))
 
 with open(file_all_authors_save,'w') as file_save:
     for author,org in normalize_authors:
