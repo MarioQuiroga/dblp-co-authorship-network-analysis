62402c832df0e771326be4ecc0b8bc316bc2abd6
agrup <agustinrodriguez206@gmail.com>
2019-11-12 19:14:49 -0300

    change facebook file name

--- scrapers/google_researchers_scraper.py
+++ scrapers/google_researchers_scraper.py
@@ -7,10 +7,7 @@ import time
 import re
 import unicodedata as ud
 
-# from selenium.common.exceptions import TimeoutException
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver.common.by import By
+
 
 from selenium import webdriver
 from bs4 import BeautifulSoup
