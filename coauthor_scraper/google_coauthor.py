0f47a091c65afee4e0923daf26cb66b49258b1a5
agrup <agustinrodriguez206@gmail.com>
2019-11-26 06:14:20 -0300

    googlr authors

--- coauthor_scraper/google_coauthor.py
+++ coauthor_scraper/google_coauthor.py
@@ -24,10 +24,11 @@ dic3=dict(list(authors_titles_json.items())[601:900])
 dic4=dict(list(authors_titles_json.items())[901:1200])
 dic5=dict(list(authors_titles_json.items())[1201:1500])
 dic6=dict(list(authors_titles_json.items())[1501:1800])
-dic7=dict(list(authors_titles_json.items())[1801:])
-dic8 = dict(list(authors_titles_json.items())[1761:1800])
-#listos dic1 dic2 dic3 dic 4 dic5 dic6
-dics=[dic1, dic2, dic3, dic4, dic5, dic6,dic7]
+dic7=dict(list(authors_titles_json.items())[1801:2100])
+dic8=dict(list(authors_titles_json.items())[2101:])
+dic9 = dict(list(authors_titles_json.items())[1761:1800])
+#listos dic1 dic2 dic3 dic4 dic5 dic6 dic7
+dics=[dic8]
 
 with open(google_coauthors_edges_path,'a')as intitute_csv_file:
 #for author,coauthor in authors_json:
